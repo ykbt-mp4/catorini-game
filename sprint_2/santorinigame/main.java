9c2546261e87fc4ff137d68020f09976965d1986
cahn0001 <cahn0001@student.monash.edu>
2025-04-26 19:42:08 +1000

    Refactor Main.java to remove redundant code - Remove setTitle() method and set name in new JFrame().

--- sprint_2/santorinigame/main.java
+++ sprint_2/santorinigame/main.java
@@ -8,10 +8,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame(); // Create a new JFrame
+        JFrame frame = new JFrame("Santorini Game"); // Create a new JFrame
         frame.setSize(1920, 1080); // Set width and height
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close operation
-        frame.setTitle("Santorini Game"); // Set title
         frame.setLayout(new GridLayout(5, 5));
 
         for (int i = 0; i < 25; i++) {

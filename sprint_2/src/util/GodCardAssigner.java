7791342ce92af1e846b24e9e433e44ef57c0d10e
whylim3 <yfesshaye01@gmail.com>
2025-04-30 00:42:50 +1000

    Change god card assigner so that players get unique god cards (no duplicates)

--- sprint_2/src/util/GodCardAssigner.java
+++ sprint_2/src/util/GodCardAssigner.java
@@ -4,21 +4,23 @@ import actors.Demeter;
 import actors.God;
 import actors.Player;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.List;
-import java.util.Arrays;
 
 public class GodCardAssigner {
 
-        private static final List<God> availableGods = Arrays.asList(
-                new Artemis(),
-                new Demeter()
-        );
+    private static final List<God> availableGods = new ArrayList<>(List.of(
+            new Artemis(),
+            new Demeter()
+    ));
 
-        public static void assignRandomGod(Player player) {
-            Random random = new Random();
-            God god = availableGods.get(random.nextInt(availableGods.size()));
-            player.setGodCard(god);
+    public static void assignRandomGod(Player player) {
+        if (availableGods.isEmpty()) {
+            throw new IllegalStateException("No more god cards available to assign.");
         }
+        Random random = new Random();
+        God god = availableGods.remove(random.nextInt(availableGods.size()));
+        player.setGodCard(god);
     }
-
+}

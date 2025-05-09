6795fb6e46066ed4195ad84b7420cdfd5112dee8
Ethan Tse <etse0007@student.monash.edu>
2025-05-09 18:26:37 +1000

    Revert "Merge branch 'main' into feature/build"
    This reverts commit f39d70db52c589ec7dc6562271549b667b35d004, reversing
    changes made to 7e87af856db1c163067f80c199556a8b20cb3cba.

--- sprint_2/src/Main.java
+++ sprint_2/src/Main.java
@@ -1,6 +1,5 @@
 import actors.Player;
 import main.GamePanel;
-import main.SidePanel;
 import util.GodCardAssigner;
 
 import javax.swing.*;
@@ -25,18 +24,11 @@ public class Main {
         System.out.println("Player 2: "+ player2.getGodCard().getName());
 
         GamePanel gamePanel = new GamePanel(player1, player2);
-        SidePanel sidePanel = new SidePanel(player1, player2);
 
-        // Initialise game
         gamePanel.setWorkerPos();
-        gamePanel.gameStart();
 
         window.add(gamePanel, BorderLayout.WEST);
-        window.add(sidePanel, BorderLayout.EAST);
         window.setVisible(true);
-
-
-
 
     }
 }

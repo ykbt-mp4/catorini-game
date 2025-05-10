a7069ce4c0d01969993adb1a989541baa0cf39ef
cahn0001 <cahn0001@student.monash.edu>
2025-05-10 23:36:36 +1000

    Add Javadoc for God, Artemis, and Demeter classes. - Documented God base class and Artemis/Demeter subclasses using Google-style Javadoc

--- sprint_2/src/actors/gods/Artemis.java
+++ sprint_2/src/actors/gods/Artemis.java
@@ -1,17 +1,29 @@
 package actors.gods;
 
+/**
+ * Represents the Artemis God card, which has a unique build and move behaviour.
+ */
 public class Artemis extends God {
 
+    /**
+     * Constructs an Artemis God and loads its image.
+     */
     public Artemis() {
         super("Artemis");
         loadGodImage("/godImages/artemis.png");
     }
 
+    /**
+     * Executes Artemis-specific build behaviour.
+     */
     @Override
     public void build() {
         // Artemis-specific build behavior
     }
 
+    /**
+     * Executes Artemis-specific move behaviour.
+     */
     @Override
     public void move() {
         // Artemis-specific move behavior
